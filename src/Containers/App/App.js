638f4e8cedb5a4da7c9a05a2d827b27668a9312c
Javs C <javcolin1@gmail.com>
2020-12-09 17:56:33 -0800

    class component

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,23 +1,30 @@
 import './App.scss';
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
 import CardList from '../../Components/CardList/CardList';
 // import ErrorBoundary from '../Components/ErrorBoundary';
 import SearchBox from '../../Components/SearchBox/SearchBox';
 // import Scroll from '../Components/Scroll';
+import axios from 'axios';
+import Pagination from 'react-js-pagination';
 
 class App extends React.Component {
-	constructor () {
-		super();
+	constructor (props) {
+		super(props);
 		this.state = {
-			 pokemons: [],
-            searchfield:''
+			pokemons: [],
+            searchfield:'',
+            offset: 0,
+      		data: [],
+     		perPage: 5,
+      		currentPage: 0,
+      		activePage: 1
 		}
 	}
 
 
 	componentDidMount() {
     const interval = {
-      limit: 20,
+      limit: 25,
       offset: 1
     }
     const Pokedex = require('pokedex-promise-v2');
@@ -38,6 +45,14 @@ class App extends React.Component {
         this.setState({ searchfield: event.target.value })
     }
 
+
+    handlePageChange(pageNumber) {
+    console.log(`active page is ${pageNumber}`);
+    this.setState({activePage: pageNumber});
+  }
+ 
+
+
 	render() {
 		const { pokemons, searchfield } = this.state;
         const filteredPokemons = pokemons.filter(pokemon => pokemon.forms[0].name.toLowerCase().includes(searchfield.toLowerCase()));
@@ -52,6 +67,15 @@ class App extends React.Component {
                         <SearchBox searchChange={this.onSearchChange}/>                       
                         <CardList pokemons={filteredPokemons}/>
                     </div>
+                      <div>
+       				 	<Pagination
+				          activePage={this.state.activePage}
+				          itemsCountPerPage={10}
+				          totalItemsCount={450}
+				          pageRangeDisplayed={5}
+				          onChange={this.handlePageChange.bind(this)}
+				        />
+				      </div>
                     <footer className="pv2 ph3 ph5-m ph6-l mid-gray">
                       <small className="f6 db tc">© 2020 Made by <b className="ttu">Javier Colin</b></small>
                     </footer>
